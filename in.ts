d24e3bee3c58cdd2d2b43b8ade4fde3a49e0296e
Jan <timmyjan46@gmail.com>
2022-11-16 21:49:19 +0000

    first commit

---
+++ in.ts
@@ -0,0 +1,50 @@
+
+{
+// script for out-buttonn52.js
+ const SELF = document.querySelector("[melte-id='buttonn52']")
+
+    SELF.querySelector("button").addEventListener("click", function() {
+        console.log(SELF.querySelector("button").innerText)
+        count += 1
+        SELF.querySelector("button").innerText = (count).toString()
+    })
+    SELF.querySelector("button").innerText = (count).toString()
+    //Using js to update very page load is too slow and causes an update which is very ugly
+
+}{
+// script for out-buttonn71.js
+ const SELF = document.querySelector("[melte-id='buttonn71']")
+
+    SELF.querySelector("button").addEventListener("click", function() {
+        console.log(SELF.querySelector("button").innerText)
+        count += 1
+        SELF.querySelector("button").innerText = (count).toString()
+    })
+    SELF.querySelector("button").innerText = (count).toString()
+    //Using js to update very page load is too slow and causes an update which is very ugly
+
+}{
+// script for out-buttonn90.js
+ const SELF = document.querySelector("[melte-id='buttonn90']")
+
+    SELF.querySelector("button").addEventListener("click", function() {
+        console.log(SELF.querySelector("button").innerText)
+        count += 1
+        SELF.querySelector("button").innerText = (count).toString()
+    })
+    SELF.querySelector("button").innerText = (count).toString()
+    //Using js to update very page load is too slow and causes an update which is very ugly
+
+}{
+// script for out-buttonn109.js
+ const SELF = document.querySelector("[melte-id='buttonn109']")
+
+    SELF.querySelector("button").addEventListener("click", function() {
+        console.log(SELF.querySelector("button").innerText)
+        count += 1
+        SELF.querySelector("button").innerText = (count).toString()
+    })
+    SELF.querySelector("button").innerText = (count).toString()
+    //Using js to update very page load is too slow and causes an update which is very ugly
+
+}
---
+++ routes/thing/out.js
@@ -0,0 +1,37 @@
+// in.ts
+{
+  const SELF = document.querySelector("[melte-id='buttonn52']");
+  SELF.querySelector("button").addEventListener("click", function() {
+    console.log(SELF.querySelector("button").innerText);
+    count += 1;
+    SELF.querySelector("button").innerText = count.toString();
+  });
+  SELF.querySelector("button").innerText = count.toString();
+}
+{
+  const SELF = document.querySelector("[melte-id='buttonn71']");
+  SELF.querySelector("button").addEventListener("click", function() {
+    console.log(SELF.querySelector("button").innerText);
+    count += 1;
+    SELF.querySelector("button").innerText = count.toString();
+  });
+  SELF.querySelector("button").innerText = count.toString();
+}
+{
+  const SELF = document.querySelector("[melte-id='buttonn90']");
+  SELF.querySelector("button").addEventListener("click", function() {
+    console.log(SELF.querySelector("button").innerText);
+    count += 1;
+    SELF.querySelector("button").innerText = count.toString();
+  });
+  SELF.querySelector("button").innerText = count.toString();
+}
+{
+  const SELF = document.querySelector("[melte-id='buttonn109']");
+  SELF.querySelector("button").addEventListener("click", function() {
+    console.log(SELF.querySelector("button").innerText);
+    count += 1;
+    SELF.querySelector("button").innerText = count.toString();
+  });
+  SELF.querySelector("button").innerText = count.toString();
+}
